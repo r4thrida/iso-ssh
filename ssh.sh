5e85dc6f9ba0cd970e48e857b0672212d3e4fcf2
r4thrida <31121043+r4thrida@users.noreply.github.com>
2021-07-25 07:31:26 -0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Enter passwd for remote login"
+passwd
 sed -i '/#PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config || echo "changed sshd_config to enable rooting" && echo "sed failed to make change to sshd."
 systemctl start sshd.service
